8633f14b30bbd40effceb7a880611b54569f687b
Tran Gia Vong <s4012094@rmit.edu.vn>
2024-09-10 18:08:53 +0700

    add available years for population

--- src/main/java/app/ClimateApp.java
+++ src/main/java/app/ClimateApp.java
@@ -6,11 +6,25 @@ public class ClimateApp {
     // private static final String DATABASE = "jdbc:sqlite:C:/Users/ASUS/Downloads/climateData.db";
     public static void main(String[] args) {
         JDBCClimate jdbc = new JDBCClimate();
+        
         System.out.println("Available Years for temperature: ");
         ArrayList<Integer> availableTempYear = new ArrayList<Integer>();
         availableTempYear = jdbc.getAvailableYearTemp();
         for (Integer year : availableTempYear) {
             System.out.println(year);
         }
+
+        System.out.println("----------------------------------------------------------------------------");
+
+        System.out.println("Available Years for population: ");
+        ArrayList<Integer> availablePopulationYear = new ArrayList<Integer>();
+        availablePopulationYear = jdbc.getAvailableYearPopulation();
+        for (Integer year : availablePopulationYear) {
+            System.out.println(year);
+        }
+
+        System.out.println("----------------------------------------------------------------------------");
+
+        
     }
 }
--- src/main/java/app/JDBCClimate.java
+++ src/main/java/app/JDBCClimate.java
@@ -12,7 +12,7 @@ public class JDBCClimate {
     private static final String DATABASE = "jdbc:sqlite:C:/Users/ASUS/Downloads/climateData.db";
 
     public JDBCClimate() {
-        System.out.println("Object created: ");
+        System.out.println("Object created: \n");
     }
 
     //Get the available year for Temperature
@@ -43,6 +43,60 @@ public class JDBCClimate {
             }
 
             String query2 = "SELECT MAX(year) AS year FROM global_yearly_land_temp;";
+
+            //Get Result
+            ResultSet result2 = statement.executeQuery(query2);
+
+            while (result2.next()) {
+                Integer year2 = result2.getInt("year");
+                availableYears.add(year2);
+            }
+
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+        return availableYears;
+    }
+
+    public ArrayList<Integer> getAvailableYearPopulation() {
+        ArrayList<Integer> availableYears = new ArrayList<Integer>();
+        Connection connection = null;
+
+        try {
+            // Connect to JDBC database
+            connection = DriverManager.getConnection(DATABASE);
+
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            // The Query
+            String query = "SELECT MIN(year) AS year FROM dim_population;";
+            
+            // Get Result
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                Integer year = result.getInt("year");
+
+                // Store the movieName in the ArrayList to return
+                availableYears.add(year);
+            }
+
+            String query2 = "SELECT MAX(year) AS year FROM dim_population;";
 
             //Get Result
             ResultSet result2 = statement.executeQuery(query2);
